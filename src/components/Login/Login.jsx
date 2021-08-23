2505d5f33d6321e79bfbc4fb66b50dbc72cca154
6urrp <sndrtasic@gmail.com>
2021-08-23 13:50:58 +0200

    Login and reset button functionality added, as well as helper function to hide the password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { hidePass } from "../../utilities/hidePassword";
 
 import "./Login.css"
 
 export const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [pass, setPass] = useState("");
+
+    const getUsername = (e) => {
+        setUsername(e.target.value);
+        return username;
+    }
+
+    const getPassword = (e) => {
+        setPass(e.target.value);
+        return pass;
+    }
+
+    const onLogin = (e) => {
+        e.preventDefault();
+        let alertText = `
+        Username: ${username}
+        Password: ${hidePass(pass)}
+        `;
+        alert(alertText);
+    }
+
+    const onReset = () => {
+        setUsername("");
+        setPass("");
+    }
+
+
+
     return (
-        <div class="login-container">
+        <div className="login-container">
             <h1>Login</h1>
-            <form method="post">
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
-                <button type="Submit" id="login-button">login</button>
-                <button type="Submit" id="reset-button">reset</button>
+            <form method="get">
+                <input type="text" placeholder="username" onChange={getUsername} value={username} />
+                <input type="password" placeholder="password" onChange={getPassword} value={hidePass(pass)}/>
+                <button type="Submit" id="login-button" onClick={onLogin}>login</button>
+                <button type="Submit" id="reset-button" onClick={onReset}>reset</button>
             </form>
         </div>
     )
